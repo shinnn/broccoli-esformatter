6e405b39e9c201eca1275182807c161ca90b0272
shinnn <snnskwtnb@gmail.com>
2014-02-28 20:09:46 +0900

    support esformatter configuration file

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
+var fs = require('fs');
 var Filter = require('broccoli-filter');
 var esformatter = require('esformatter');
+
+function assign(destObj, srcObj) {
+  for (var attrname in srcObj) {
+    destObj[attrname] = srcObj[attrname];
+  }
+  return destObj;
+}
 
 function EsFormatterFilter(inputTree, options) {
   if (!(this instanceof EsFormatterFilter)) {
@@ -9,7 +17,7 @@ function EsFormatterFilter(inputTree, options) {
   }
   
   this.inputTree = inputTree;
-  this.options = options || {};
+  this.options = assign({config: true}, options);
 }
 
 EsFormatterFilter.prototype = Object.create(Filter.prototype);
@@ -19,7 +27,30 @@ EsFormatterFilter.prototype.extensions = ['js'];
 EsFormatterFilter.prototype.targetExtension = 'js';
 
 EsFormatterFilter.prototype.processString = function(str) {
-  return esformatter.format(str, this.options);
+  var baseOpts = {};
+  var configFilePath;
+  var pkg;
+  try {
+    pkg = require('./package.json');
+  } catch (e) {}
+  
+  if (this.options.config === true) {
+    try {
+      baseOpts = JSON.parse(fs.readFileSync('.esformatter'));
+    } catch (e) {
+      if (pkg && pkg.esformatter) {
+        baseOpts = pkg.esformatter;
+      }
+    }
+  } else if (typeof this.options.config === 'string') {
+    var baseOpts = JSON.parse(fs.readFileSync(this.options.config));
+    // package.json
+    if (typeof baseOpts.esformatter === 'object') {
+      baseOpts = baseOpts.esformatter;
+    }
+  }
+    
+  return esformatter.format(str, assign(baseOpts, this.options));
 };
 
 module.exports = EsFormatterFilter;
