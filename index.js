eef5f6f4c51a9a51f43e34f552416044368fe74e
shinnn <snnskwtnb@gmail.com>
2014-06-23 00:22:11 +0900

    use object-assign instead of handmade assign

--- index.js
+++ index.js
@@ -3,13 +3,7 @@
 var fs = require('fs');
 var Filter = require('broccoli-filter');
 var esformatter = require('esformatter');
-
-function assign(destObj, srcObj) {
-  for (var attrname in srcObj) {
-    destObj[attrname] = srcObj[attrname];
-  }
-  return destObj;
-}
+var objectAssign = require('object-assign');
 
 function EsFormatterFilter(inputTree, options) {
   if (!(this instanceof EsFormatterFilter)) {
@@ -17,7 +11,7 @@ function EsFormatterFilter(inputTree, options) {
   }
 
   this.inputTree = inputTree;
-  this.options = assign({config: true}, options);
+  this.options = objectAssign({config: true}, options || {});
 }
 
 EsFormatterFilter.prototype = Object.create(Filter.prototype);
@@ -29,7 +23,7 @@ EsFormatterFilter.prototype.targetExtension = 'js';
 EsFormatterFilter.prototype.processString = function(str) {
   var baseOpts = {};
   var configFilePath;
-  
+
   if (this.options.config === true) {
     var message = '';
 
@@ -43,10 +37,10 @@ EsFormatterFilter.prototype.processString = function(str) {
         } else if (typeof pkgConfig.esformatter === 'object') {
           baseOpts = pkgConfig.esformatter;
         } else {
-          message = "'esformatter' property in package.json is not an object.";
+          message = '"esformatter" property in the package.json is not an object.';
         }
       } catch (packageJsonReadError) {
-        message = 'Cannot find esformatter configuration file.';
+        message = 'esformatter configuration file not found.';
       }
     }
 
@@ -61,8 +55,8 @@ EsFormatterFilter.prototype.processString = function(str) {
       baseOpts = baseOpts.esformatter;
     }
   }
-  
-  return esformatter.format(str, assign(baseOpts, this.options));
+
+  return esformatter.format(str, objectAssign(baseOpts, this.options));
 };
 
 module.exports = EsFormatterFilter;
